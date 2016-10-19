758bcb252a125761890b461729b99899efc1c182
mapserver2007 <mapserver2007@gmail.com>
2016-10-20 00:47:18 +0900

    テキストを保存するように修正

--- tumblr_config.rb
+++ tumblr_config.rb
@@ -8,24 +8,29 @@ require 'parallel_runner'
 
 def create_list(keyword, tag)
   data = {
-    :keyword => keyword,
-    :tag => tag,
-    :img => []
+    keyword: keyword,
+    tag: tag,
+    contents: []
   }
   agent = Mechanize.new
   agent.user_agent_alias = 'Mac Mozilla'
   agent.read_timeout = 10
   site = agent.get(URI.encode("https://www.tumblr.com/search/#{tag}/recent"))
-  tumblr_lines = (site/'//img[@class="photo"]')
-  tumblr_lines.each do |tumblr_line|
+
+  contents = (site/'//section[@class="post_content"]')
+  contents.each do |content|
     begin
-      imgUrl = tumblr_line.get_attribute("src")
-      imgUrl = tumblr_line.get_attribute("data-src") unless /^https?.+/ =~ imgUrl
-      data[:img] << imgUrl
+      text = content.search('div[class="post_body"]/p[1]').text.gsub(/\n/, "")
+      content.search('img[class="photo"]').each do |img|
+        imgUrl = img.get_attribute("src")
+        imgUrl = img.get_attribute("data-src") unless /^https?.+/ =~ imgUrl
+        data[:contents] << {img: imgUrl, text: text}
+      end
     rescue => e
-      logger.error e.message
+      yield e.message
     end
   end
+
   data
 end
 
@@ -71,33 +76,46 @@ logger.info "clean done."
 
 # 声優のあだ名一覧を取得(女性声優のみ)
 # ある日構成が変わったり、ページが削除されたら頑張って対応する
-agent = Mechanize.new
-agent.user_agent_alias = 'Mac Mozilla'
-agent.read_timeout = 10
-site = agent.get(URI.encode("http://dic.nicovideo.jp/a/声優の愛称一覧"))
-list = (site/'//*[@id="article"]/table[2]/tbody/tr/td/ul/li').inject([]) {|l, e| l << e}
-list.each do |line|
-  if /(.+)\uFF08(.+)\uFF09/ =~ line.inner_text
-    $1.split("\u30FB").each do |nickname|
-      save(config, create_list(nickname, $2))
+begin
+  agent = Mechanize.new
+  agent.user_agent_alias = 'Mac Mozilla'
+  agent.read_timeout = 10
+  site = agent.get(URI.encode("http://dic.nicovideo.jp/a/声優の愛称一覧"))
+  list = (site/'//*[@id="article"]/table[2]/tbody/tr/td/ul/li').inject([]) {|l, e| l << e}
+  list.each do |line|
+    if /(.+)\uFF08(.+)\uFF09/ =~ line.inner_text
+      $1.split("\u30FB").each do |nickname|
+        save(config, create_list(nickname, $2) {|message|
+          logger.error message
+        })
+      end
+      save(config, create_list($2, $2))
+      logger.info "#{$2} done."
     end
-    save(config, create_list($2, $2))
-    logger.info "#{$2} done."
   end
-end
 
-# カスタムルール(特によく使うもの)
-file = File.dirname(__FILE__) + "/config/origin_rules.yml"
-origin_rules = YAML.load_file(file)
-origin_rules["list"].each do |keyword|
-  save(config, create_list(keyword, keyword))
-  logger.info "#{keyword} done."
-end
-origin_rules["alias"].each do |tag, keywords|
-  keywords.each do |keyword|
-    save(config, create_list(keyword, tag))
+  # カスタムルール(特によく使うもの)
+  file = File.dirname(__FILE__) + "/config/origin_rules.yml"
+  origin_rules = YAML.load_file(file)
+  origin_rules["list"].each do |keyword|
+    save(config, create_list(keyword, keyword) {|message|
+      logger.error message
+    })
+    logger.info "#{keyword} done."
   end
-  logger.info "#{tag} done."
-end
+  origin_rules["alias"].each do |tag, keywords|
+    keywords.each do |keyword|
+      save(config, create_list(keyword, tag) {|message|
+        logger.error message
+      })
+    end
+    logger.info "#{tag} done."
+  end
 
-logger.info "update done."
+  logger.info "update success."
+
+rescue => e
+  logger.error e.message
+  logger.error e.backtrace.join("\n")
+  logger.info "update failure."
+end
